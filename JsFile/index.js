8cfd95d5161359fbcda420bb35e234db78c4c55c
UJJWAL KUMAR KESHRI <64123376+coolujjwal@users.noreply.github.com>
2021-01-14 16:08:22 +0530

    Add files via upload

---
+++ JsFile/index.js
@@ -0,0 +1,266 @@
+class List {
+  constructor ({
+    itemText1,
+    itemText2,
+    link1,
+    link2
+  }) {
+    this.leftOne = this.createDiv({
+      className: "item1",
+      text: itemText1
+    });
+    this.leftTwo = this.createDiv({
+      className: "item2",
+      text: itemText2
+    });
+    this.leftItemSource = this.createDiv({
+      className: "left-item",
+      childElements: [this.leftOne,this.leftTwo]
+    });
+    this.l3 = this.createAttributeTag({
+      href: link1,
+      className: "btn0",
+      text: "Link1"
+    });
+    this.l4 = this.createAttributeTag({
+      href: link2,
+      className: "btn0",
+      text: "Link2"
+    });
+    this.rightThree = this.createDiv({
+      className: "item3",
+      childElements: [this.l3]
+    });
+    this.rightFour = this.createDiv({
+      className: "item4",
+      childElements: [this.l4]
+    });
+    this.rightItemSource = this.createDiv({
+      className: "right-item",
+      childElements: [this.rightThree,this.rightFour]
+    });
+    this.rowItemSource = this.createDiv({
+      className: "row-item",
+      childElements: [this.leftItemSource,this.rightItemSource]
+    });
+  }
+  createAttributeTag({ href = "", target = "_blank",className=null, text }) {
+    const tagDocument = document.createElement("a");
+    tagDocument.href = href;
+    tagDocument.target = target;
+    tagDocument.className=className;
+    const tagTextElement = document.createTextNode(text);
+    tagDocument.appendChild(tagTextElement);
+    return tagDocument;
+  }
+  createDiv({ className = null,text="", childElements = [] }) {
+    const divElement = document.createElement("div");
+    divElement.innerHTML=text;
+    if (className) divElement.className = className;
+    childElements.forEach(element => {
+      divElement.appendChild(element);
+    });
+    return divElement;
+  }
+  createRowCard() {
+    return this.rowItemSource;
+  }
+}
+
+
+const rowItem1 = () => {
+  topic1.map(list => {
+      const projectCard = new List({
+        itemText1: list.itemText1,
+        itemText2: list.itemText2,
+        link1: list.link1,
+        link2: list.link2
+      }).createRowCard();
+      document.getElementById("post-wrapper-id1").appendChild(projectCard);
+    }
+  );
+};
+const rowItem2 = () => {
+  topic2.map(list => {
+      const projectCard = new List({
+        itemText1: list.itemText1,
+        itemText2: list.itemText2,
+        link1: list.link1,
+        link2: list.link2
+      }).createRowCard();
+      document.getElementById("post-wrapper-id2").appendChild(projectCard);
+    }
+  );
+};
+const rowItem3 = () => {
+  topic3.map(list => {
+      const projectCard = new List({
+        itemText1: list.itemText1,
+        itemText2: list.itemText2,
+        link1: list.link1,
+        link2: list.link2
+      }).createRowCard();
+      document.getElementById("post-wrapper-id3").appendChild(projectCard);
+    }
+  );
+};
+const rowItem4 = () => {
+  topic4.map(list => {
+      const projectCard = new List({
+        itemText1: list.itemText1,
+        itemText2: list.itemText2,
+        link1: list.link1,
+        link2: list.link2
+      }).createRowCard();
+      document.getElementById("post-wrapper-id4").appendChild(projectCard);
+    }
+  );
+};
+const rowItem5 = () => {
+  topic5.map(list => {
+      const projectCard = new List({
+        itemText1: list.itemText1,
+        itemText2: list.itemText2,
+        link1: list.link1,
+        link2: list.link2
+      }).createRowCard();
+      document.getElementById("post-wrapper-id5").appendChild(projectCard);
+    }
+  );
+};
+const rowItem6 = () => {
+  topic6.map(list => {
+      const projectCard = new List({
+        itemText1: list.itemText1,
+        itemText2: list.itemText2,
+        link1: list.link1,
+        link2: list.link2
+      }).createRowCard();
+      document.getElementById("post-wrapper-id6").appendChild(projectCard);
+    }
+  );
+};
+const rowItem7 = () => {
+  topic7.map(list => {
+      const projectCard = new List({
+        itemText1: list.itemText1,
+        itemText2: list.itemText2,
+        link1: list.link1,
+        link2: list.link2
+      }).createRowCard();
+      document.getElementById("post-wrapper-id7").appendChild(projectCard);
+    }
+  );
+};
+const rowItem8 = () => {
+  topic8.map(list => {
+      const projectCard = new List({
+        itemText1: list.itemText1,
+        itemText2: list.itemText2,
+        link1: list.link1,
+        link2: list.link2
+      }).createRowCard();
+      document.getElementById("post-wrapper-id8").appendChild(projectCard);
+    }
+  );
+};
+const rowItem9 = () => {
+  topic9.map(list => {
+      const projectCard = new List({
+        itemText1: list.itemText1,
+        itemText2: list.itemText2,
+        link1: list.link1,
+        link2: list.link2
+      }).createRowCard();
+      document.getElementById("post-wrapper-id9").appendChild(projectCard);
+    }
+  );
+};
+const rowItem10 = () => {
+  topic10.map(list => {
+      const projectCard = new List({
+        itemText1: list.itemText1,
+        itemText2: list.itemText2,
+        link1: list.link1,
+        link2: list.link2
+      }).createRowCard();
+      document.getElementById("post-wrapper-id10").appendChild(projectCard);
+    }
+  );
+};
+const rowItem11 = () => {
+  topic11.map(list => {
+      const projectCard = new List({
+        itemText1: list.itemText1,
+        itemText2: list.itemText2,
+        link1: list.link1,
+        link2: list.link2
+      }).createRowCard();
+      document.getElementById("post-wrapper-id11").appendChild(projectCard);
+    }
+  );
+};
+const rowItem12 = () => {
+  topic12.map(list => {
+      const projectCard = new List({
+        itemText1: list.itemText1,
+        itemText2: list.itemText2,
+        link1: list.link1,
+        link2: list.link2
+      }).createRowCard();
+      document.getElementById("post-wrapper-id12").appendChild(projectCard);
+    }
+  );
+};
+const rowItem13 = () => {
+  topic13.map(list => {
+      const projectCard = new List({
+        itemText1: list.itemText1,
+        itemText2: list.itemText2,
+        link1: list.link1,
+        link2: list.link2
+      }).createRowCard();
+      document.getElementById("post-wrapper-id13").appendChild(projectCard);
+    }
+  );
+};
+const rowItem14 = () => {
+  topic14.map(list => {
+      const projectCard = new List({
+        itemText1: list.itemText1,
+        itemText2: list.itemText2,
+        link1: list.link1,
+        link2: list.link2
+      }).createRowCard();
+      document.getElementById("post-wrapper-id14").appendChild(projectCard);
+    }
+  );
+};
+const rowItem15 = () => {
+  topic15.map(list => {
+      const projectCard = new List({
+        itemText1: list.itemText1,
+        itemText2: list.itemText2,
+        link1: list.link1,
+        link2: list.link2
+      }).createRowCard();
+      document.getElementById("post-wrapper-id15").appendChild(projectCard);
+    }
+  );
+};
+
+rowItem1();
+rowItem2();
+rowItem3();
+rowItem4();
+rowItem5();
+rowItem6();
+rowItem7();
+rowItem8();
+rowItem9();
+rowItem10();
+rowItem11();
+rowItem12();
+rowItem13();
+rowItem14();
+rowItem15();
